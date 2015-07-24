c37f1bc96aa7e5795053622479b1c528ecb64b0f
David Seaman <david.seaman@hapara.com>
2015-07-24 13:36:38 +1200

    Forgot script

---
+++ run.sh
@@ -0,0 +1,17 @@
+mkdir -p ~/.vim/autoload ~/.vim/bundle 
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir ~/.vim/colors
+
+cd ~/.vim
+git clone https://github.com/sickill/vim-monokai.git
+ln -s ~/.vim/vim-monokai/colors/monokai.vim ~/.vim/colors/monokai.vim
+git clone https://github.com/kchmck/vim-coffee-script.git ~/.vim/bundle/vim-coffee-script/
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree/
+git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter/
+
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+curl -Sso ~/.vim/autoload/pathogen.vim \
+    https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+
+
