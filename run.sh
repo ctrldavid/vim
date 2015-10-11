af4839fd7328dd876b6b658d324fff0f49736a7e
David Seaman <david.seaman@hapara.com>
2015-10-12 12:22:16 +1300

    Add strange colour scheme

--- run.sh
+++ run.sh
@@ -5,10 +5,15 @@ mkdir ~/.vim/colors
 cd ~/.vim
 git clone https://github.com/sickill/vim-monokai.git
 ln -s ~/.vim/vim-monokai/colors/monokai.vim ~/.vim/colors/monokai.vim
+
+git clone git@github.com:strange/strange.vim.git
+ln -s ~/.vim/strange.vim/colors/strange.vim ~/.vim/colors/strange.vim
+
+
+
 git clone https://github.com/kchmck/vim-coffee-script.git ~/.vim/bundle/vim-coffee-script/
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree/
 git clone git://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter/
-
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
 curl -Sso ~/.vim/autoload/pathogen.vim \
